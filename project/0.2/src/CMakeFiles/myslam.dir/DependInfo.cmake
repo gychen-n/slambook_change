661fca13cf64a3f8ed04b18c3b8ebe685b645405
GYC <gyc@autolabor-host.autolabor-domain>
2021-05-25 10:31:21 +0800

    first commit

---
+++ project/0.2/src/CMakeFiles/myslam.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gyc/slambook-master/project/0.2/src/camera.cpp" "/home/gyc/slambook-master/project/0.2/src/CMakeFiles/myslam.dir/camera.cpp.o"
+  "/home/gyc/slambook-master/project/0.2/src/config.cpp" "/home/gyc/slambook-master/project/0.2/src/CMakeFiles/myslam.dir/config.cpp.o"
+  "/home/gyc/slambook-master/project/0.2/src/frame.cpp" "/home/gyc/slambook-master/project/0.2/src/CMakeFiles/myslam.dir/frame.cpp.o"
+  "/home/gyc/slambook-master/project/0.2/src/map.cpp" "/home/gyc/slambook-master/project/0.2/src/CMakeFiles/myslam.dir/map.cpp.o"
+  "/home/gyc/slambook-master/project/0.2/src/mappoint.cpp" "/home/gyc/slambook-master/project/0.2/src/CMakeFiles/myslam.dir/mappoint.cpp.o"
+  "/home/gyc/slambook-master/project/0.2/src/visual_odometry.cpp" "/home/gyc/slambook-master/project/0.2/src/CMakeFiles/myslam.dir/visual_odometry.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "/home/gyc/slambook-master/3rdparty/Sophus"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
